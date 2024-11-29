cee46550539484fe0e55ca8f40a11f03198c3472
YukioE <yukiod8@pm.me>
2024-11-29 18:39:10 +0100

    moved all plugin files into lua/plugins, added bufferline, catppuccin, smear-cursor

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,22 @@
+return {
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  priority = 1000, -- Load before other plugins to set colorscheme
+  config = function()
+    require('catppuccin').setup {
+      flavour = 'macchiato', -- Set the default flavour to macchiato
+      background = {
+        light = 'latte',
+        dark = 'macchiato',
+      },
+      term_colors = true,
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+      },
+    }
+    vim.cmd.colorscheme 'catppuccin' -- Apply the colorscheme
+  end,
+}
