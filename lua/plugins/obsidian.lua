a67ac608c92c522fb57025d80d3635bd07418871
YukioE <93155932+YukioE@users.noreply.github.com>
2024-12-11 21:40:56 +0100

    added Obsidian nvim

---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,15 @@
+return {
+  'epwalsh/obsidian.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  opts = {
+    workspaces = {
+      {
+        name = '1',
+        path = '~/basereality22/n e t s p h e r e/',
+      },
+    },
+  },
+}
