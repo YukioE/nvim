12aa74c0c6d7e8f44a6a98d0db1044abe9b84f70
YukioE <93155932+YukioE@users.noreply.github.com>
2024-12-15 00:32:33 +0100

    commented sleuth, added <C-d> to close buffers in telescope

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -1,23 +1,24 @@
-require('lazy').setup({
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+require('lazy').setup {
+  -- 'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
   { import = 'plugins' },
-}, {
-  ui = {
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
-    },
-  },
-})
+}
+--   , {
+--     ui = {
+--       icons = vim.g.have_nerd_font and {} or {
+--         cmd = '⌘',
+--         config = '🛠',
+--         event = '📅',
+--         ft = '📂',
+--         init = '⚙',
+--         keys = '🗝',
+--         plugin = '🔌',
+--         runtime = '💻',
+--         require = '🌙',
+--         source = '📄',
+--         start = '🚀',
+--         task = '📌',
+--         lazy = '💤 ',
+--       },
+--     },
+-- }
