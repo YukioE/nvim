fef5dc0b7ba0afb9200ee96bdc88d747073987df
YukioE <yukiod8@pm.me>
2024-12-14 21:13:53 +0100

    added barbecue plugin for breadcrumbs, changed telescope theme

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,8 +3,9 @@ return {
   name = 'catppuccin',
   priority = 1000, -- Load before other plugins to set colorscheme
   config = function()
+    -- Configure Catppuccin with the latte flavor for light and macchiato for dark
     require('catppuccin').setup {
-      flavour = 'latte', -- Set the default flavour to macchiato
+      flavour = 'latte', -- Set the default flavour to latte
       background = {
         light = 'latte',
         dark = 'macchiato',
@@ -17,9 +18,16 @@ return {
         treesitter = true,
       },
       custom_highlights = function(colors)
-        return {}
+        return {
+          TelescopeBorder = { fg = colors.base, bg = colors.base },
+          TelescopePreviewBorder = { fg = colors.base, bg = colors.base },
+          TelescopeResultsBorder = { fg = colors.base, bg = colors.base },
+          TelescopePromptBorder = { fg = colors.base, bg = colors.base },
+        }
       end,
     }
-    vim.cmd.colorscheme 'catppuccin' -- Apply the colorscheme
+
+    -- Apply the Catppuccin colorscheme after configuring
+    vim.cmd.colorscheme 'catppuccin'
   end,
 }
